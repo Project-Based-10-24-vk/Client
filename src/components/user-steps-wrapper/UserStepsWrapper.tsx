5708be5b5237efbc98d00ded9d812d2b777ca709
unknown <romanovanmih@gmail.com>
2024-10-16 10:41:27 +0300

    add image, title and inputs for GeneralInfoStep

--- src/components/user-steps-wrapper/UserStepsWrapper.tsx
+++ src/components/user-steps-wrapper/UserStepsWrapper.tsx
@@ -1,19 +1,18 @@
 import { FC, useEffect, useState } from 'react'
-import { useAppDispatch } from '~/hooks/use-redux'
-import { markFirstLoginComplete } from '~/redux/reducer'
-import StepWrapper from '~/components/step-wrapper/StepWrapper'
 
-import { StepProvider } from '~/context/step-context'
-
-import GeneralInfoStep from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep'
 import AddPhotoStep from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep'
-import SubjectsStep from '~/containers/tutor-home-page/subjects-step/SubjectsStep'
+import GeneralInfoStep from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep'
 import LanguageStep from '~/containers/tutor-home-page/language-step/LanguageStep'
-
+import SubjectsStep from '~/containers/tutor-home-page/subjects-step/SubjectsStep'
+import StepWrapper from '~/components/step-wrapper/StepWrapper'
 import {
-  tutorStepLabels,
-  initialValues
+  initialValues,
+  studentStepLabels,
+  tutorStepLabels
 } from '~/components/user-steps-wrapper/constants'
+import { StepProvider } from '~/context/step-context'
+import { markFirstLoginComplete } from '~/redux/reducer'
+import { useAppDispatch } from '~/hooks/use-redux'
 import { student } from '~/constants'
 
 interface UserStepsWrapperProps {
@@ -39,7 +38,7 @@ const UserStepsWrapper: FC<UserStepsWrapperProps> = ({ userRole }) => {
     <AddPhotoStep key='4' />
   ]
 
-  const stepLabels = userRole === student ? '' : tutorStepLabels
+  const stepLabels = userRole === student ? studentStepLabels : tutorStepLabels
 
   return (
     <StepProvider initialValues={initialValues} stepLabels={stepLabels}>
