8fb021930ccdee4f19cd65e353b34a3d8039a44a
melyasanya <werbolom@gmail.com>
2024-10-16 15:10:32 +0300

    added photo and created select component for subject step

---
+++ src/components/subject-step-select/SubjectStepSelect.tsx
@@ -0,0 +1,3 @@
+export const SubjectStepSelect = () => {
+  return <div></div>
+}
--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,12 +1,16 @@
 import Box from '@mui/material/Box'
-
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
+import { SubjectStepSelect } from '~/components/subject-step-select/SubjectStepSelect'
+import img from '~/assets/img/tutor-home-page/become-tutor/study-category.svg'
 
 const SubjectsStep = ({ btnsBox }) => {
   return (
     <Box sx={styles.container}>
-      <Box sx={styles.rigthBox}>
-        Subjects step
+      <Box sx={styles.imgContainer}>
+        <Box component='img' src={img} sx={styles.img} />
+      </Box>
+      <Box sx={styles.rightBox}>
+        <SubjectStepSelect />
         {btnsBox}
       </Box>
     </Box>
