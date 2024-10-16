145d8c0292477e3c387b2e0a73981f2618660ac4
nadraharr <nadraharr@gmail.com>
2024-10-16 11:56:37 +0300

    category dropdown

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,12 +1,16 @@
 import Box from '@mui/material/Box'
-
+import CategoryDropdown from '~/containers/category-dropdown/CategoryDropdown'
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
+import img from '~/assets/img/tutor-home-page/become-tutor/study-category.svg'
 
 const SubjectsStep = ({ btnsBox }) => {
   return (
     <Box sx={styles.container}>
+      <Box sx={styles.imgContainer}>
+        <Box component='img' src={img} sx={styles.img} />
+      </Box>
       <Box sx={styles.rigthBox}>
-        Subjects step
+        <CategoryDropdown />
         {btnsBox}
       </Box>
     </Box>
