a7d0b535a506b84f8d87b4edae2f64a7e462cfcf
unknown <romanovanmih@gmail.com>
2024-10-17 11:44:22 +0300

    implement general-info-step

--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.jsx
@@ -3,18 +3,19 @@ import { useTranslation } from 'react-i18next'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { styles } from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles'
-import AppSelect from '~/components/app-select/AppSelect'
 import AppTextArea from '~/components/app-text-area/AppTextArea'
 import AppTextField from '~/components/app-text-field/AppTextField'
+import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete'
 import { useStepContext } from '~/context/step-context'
 import useForm from '~/hooks/use-form'
 import img from '~/assets/img/tutor-home-page/become-tutor/general-info.svg'
 
 const GeneralInfoStep = ({ btnsBox }) => {
+  const { stepData } = useStepContext()
   const { t } = useTranslation()
-  const optionsCountry = [{ title: 'errors.UNKNOWN_ERROR', value: '' }]
-  const isReadOnly = { readOnly: true }
-  // const { handleSubmit, handleInputChange, errors, data } = useForm()
+  const { handleInputChange, data } = useForm({
+    initialValues: stepData.generalInfo.data
+  })
 
   return (
     <Box sx={styles.container}>
@@ -31,28 +32,29 @@ const GeneralInfoStep = ({ btnsBox }) => {
             <AppTextField
               fullWidth
               label={t('common.labels.firstName')}
-              // onChange={handleInputChange('firstName')}
-              // value={data.firstName}
+              onChange={handleInputChange('firstName')}
+              value={data.firstName}
             />
 
             <AppTextField
               fullWidth
               label={t('common.labels.lastName')}
-              // onChange={handleInputChange('lastName')}
-              // value={data.lastName}
+              onChange={handleInputChange('lastName')}
+              value={data.lastName}
             />
           </Box>
 
           <Box sx={styles.form}>
-            <AppSelect
-              fields={optionsCountry}
-              inputProps={isReadOnly}
-              label={t('common.labels.country')}
+            <AsyncAutocomplete
+              fullWidth
+              textFieldProps={{ label: t('common.labels.country') }}
+              value={data.country}
             />
 
-            <AppSelect
-              fields={optionsCountry}
-              label={t('common.labels.city')}
+            <AsyncAutocomplete
+              fullWidth
+              textFieldProps={{ label: t('common.labels.city') }}
+              value={data.city}
             />
           </Box>
 
@@ -60,9 +62,9 @@ const GeneralInfoStep = ({ btnsBox }) => {
             fullWidth
             label={t('becomeTutor.generalInfo.textFieldLabel')}
             maxLength={70}
+            onChange={handleInputChange('professionalSummary')}
             sx={styles.textarea}
-            // value={data.professionalSummary}
-            // onChange={handleInputChange('professionalSummary')}
+            value={data.professionalSummary}
           />
         </Box>
         {btnsBox}
