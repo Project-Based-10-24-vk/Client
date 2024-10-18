b3c5bc56746cbe99cd294fededdf10a0b49f14f1
Sofia <79804918+lsofiabo@users.noreply.github.com>
2024-10-18 17:00:21 +0300

    #99 create signup popup (#127)

---
+++ src/containers/guest-home-page/signup-form/SignupForm.styles.ts
@@ -0,0 +1,14 @@
+const style = {
+  boxMultipleInputs: {
+    display: { sm: 'block', md: 'flex', lg: 'flex' },
+    gap: '16px'
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    minWidth: {
+      sm: '343px'
+    }
+  }
+}
+export default style
