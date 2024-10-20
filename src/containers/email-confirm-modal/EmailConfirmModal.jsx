2cd98db38b85d698ee3825e59e1231f0d313514c
Victor Voitenko <102621431+VoitV@users.noreply.github.com>
2024-10-20 18:32:43 +0300

    Feature/106/confirm pop (#131)
    * #106 add success form
    
    * #106 add email confirm pop-up
    
    ---------
    
    Co-authored-by: AlexApiko <Alex@apiko.com>

--- src/containers/email-confirm-modal/EmailConfirmModal.jsx
+++ src/containers/email-confirm-modal/EmailConfirmModal.jsx
@@ -1,6 +1,5 @@
 import { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
-
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import { styles } from '~/containers/email-confirm-modal/EmailConfirmModal.styles'
@@ -11,6 +10,7 @@ import { useModalContext } from '~/context/modal-context'
 import useAxios from '~/hooks/use-axios'
 import { AuthService } from '~/services/auth-service'
 import imgReject from '~/assets/img/email-confirmation-modals/not-success-icon.svg'
+import imgSuccess from '~/assets/img/email-confirmation-modals/success-icon.svg'
 
 const EmailConfirmModal = ({ confirmToken, openModal }) => {
   const { t } = useTranslation()
@@ -72,6 +72,19 @@ const EmailConfirmModal = ({ confirmToken, openModal }) => {
       </Box>
     )
   }
+
+  return (
+    <Box sx={styles.box}>
+      <ImgTitleDescription
+        img={imgSuccess}
+        style={styles}
+        title={t('modals.emailConfirm')}
+      />
+      <Button onClick={openLoginDialog} sx={styles.button} variant='contained'>
+        {t('button.goToLogin')}
+      </Button>
+    </Box>
+  )
 }
 
 export default EmailConfirmModal
