f3853473f4644dd7c8942390061551f3c740d0d4
melyasanya <werbolom@gmail.com>
2024-10-24 15:27:15 +0300

    markup

--- src/containers/tutor-home-page/add-photo-step/AddPhotoStep.jsx
+++ src/containers/tutor-home-page/add-photo-step/AddPhotoStep.jsx
@@ -1,12 +1,34 @@
-import { Box } from '@mui/material'
+import { useTranslation } from 'react-i18next'
 
+import { Box, Typography } from '@mui/material'
 import { style } from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep.style'
+import FileUploader from '~/components/file-uploader/FileUploader'
+import { ButtonVariantEnum } from '~/types'
 
 const AddPhotoStep = ({ btnsBox }) => {
+  const { t } = useTranslation()
+
   return (
     <Box sx={style.root}>
-      AddPhoto step
-      {btnsBox}
+      <Box sx={style.imgContainer}>
+        <Box sx={style.uploadBox}>
+          <Typography>Photo preview</Typography>
+        </Box>
+      </Box>
+      <Box sx={style.rigthBox}>
+        <Box>
+          <Typography sx={style.description}>
+            {t('becomeTutor.photo.description')}
+          </Typography>
+          <FileUploader
+            buttonText={t('becomeTutor.photo.button')}
+            isImages
+            sx={style.fileUploader}
+            variant={ButtonVariantEnum.Contained}
+          />
+        </Box>
+        {btnsBox}
+      </Box>
     </Box>
   )
 }
