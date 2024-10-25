556af19a3b3bb127dc51e01c2d0c9240cfb07186
Roman Nadraha <64159673+nadraharr@users.noreply.github.com>
2024-10-25 12:14:19 +0300

    offer-request block (#146)

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,7 +1,12 @@
+import OfferRequestBlock from '~/containers/find-offer/offer-request-block/OfferRequestBlock'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 
 const Categories = () => {
-  return <PageWrapper>Categories</PageWrapper>
+  return (
+    <PageWrapper>
+      <OfferRequestBlock />
+    </PageWrapper>
+  )
 }
 
 export default Categories
