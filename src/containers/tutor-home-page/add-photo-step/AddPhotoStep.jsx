552159dc3433fd00d68da620a3d9ec923c9c6f12
melyasanya <werbolom@gmail.com>
2024-10-25 14:15:28 +0300

    added functionality

--- src/containers/tutor-home-page/add-photo-step/AddPhotoStep.jsx
+++ src/containers/tutor-home-page/add-photo-step/AddPhotoStep.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { Box, Typography } from '@mui/material'
 import { style } from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep.style'
 import FileUploader from '~/components/file-uploader/FileUploader'
+import { useCreateImgUrl } from '~/hooks/use-create-img-url'
 import { ButtonVariantEnum } from '~/types'
+import { validationData } from './constants'
 
 const AddPhotoStep = ({ btnsBox }) => {
   const { t } = useTranslation()
+  const [uploadedFiles, setUploadedFiles] = useState([])
+  const [error, setError] = useState('')
+  const { urls, createUrl } = useCreateImgUrl()
+
+  const handleFilesEmit = ({ files, error }) => {
+    createUrl(files)
+
+    setUploadedFiles(files)
+    setError(error)
+  }
 
   return (
     <Box sx={style.root}>
       <Box sx={style.imgContainer}>
-        <Box sx={style.uploadBox}>
-          <Typography>Photo preview</Typography>
-        </Box>
+        {uploadedFiles.length ? (
+          <Box component='img' src={urls} sx={style.img}></Box>
+        ) : (
+          <Box sx={style.uploadBox}>
+            <Typography>{t('becomeTutor.photo.placeholder')}</Typography>
+          </Box>
+        )}
       </Box>
       <Box sx={style.rigthBox}>
         <Box>
@@ -22,8 +39,12 @@ const AddPhotoStep = ({ btnsBox }) => {
           </Typography>
           <FileUploader
             buttonText={t('becomeTutor.photo.button')}
+            emitter={handleFilesEmit}
+            initialError={error}
+            initialState={uploadedFiles}
             isImages
             sx={style.fileUploader}
+            validationData={validationData}
             variant={ButtonVariantEnum.Contained}
           />
         </Box>
