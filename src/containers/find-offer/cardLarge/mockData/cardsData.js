78cfbff423d7135cb1833d5dd31f7818136502ff
AlexApiko <Alex@apiko.com>
2024-10-29 18:07:47 +0200

    add large find-offer card

---
+++ src/containers/find-offer/cardLarge/mockData/cardsData.js
@@ -0,0 +1,49 @@
+const mockData = [
+    {
+        id: 1,
+        name: 'Jenny Wilson',
+        position: 'Senior lecturer at the Department of German Philology and Translation Department of English Philology',
+        rating: 5,
+        reviews: 10,
+        languages: ['Ukrainian', 'English'],
+        course: {
+            subject: 'German',
+            level: 'beginner - advanced'
+        },
+        description: 'Hello. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable...',
+        price: '75 UAH',
+        perHour: '/hour'
+    },
+    {
+        id: 2,
+        name: 'Esther Howard',
+        position: 'Senior lecturer at the Department of German Philology and Translation Department of English Philology',
+        rating: 5,
+        reviews: 10,
+        languages: ['Ukrainian', 'English'],
+        course: {
+            subject: 'German',
+            level: 'beginner - advanced'
+        },
+        description: 'Hello. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable...',
+        price: '75 UAH',
+        perHour: '/hour'
+    },
+    {
+        id: 3,
+        name: 'Jenny Wilson',
+        position: 'Senior lecturer at the Department of German Philology and Translation Department of English Philology',
+        rating: 5,
+        reviews: 10,
+        languages: ['Ukrainian', 'English'],
+        course: {
+            subject: 'German',
+            level: 'beginner - advanced'
+        },
+        description: 'Hello. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable...',
+        price: '75 UAH',
+        perHour: '/hour'
+    }
+];
+
+export default mockData;
