7125eb3daeb88410cc9df06f87a63dc25d148f16
Ihor Pohaidak <89802944+poggy33@users.noreply.github.com>
2024-11-04 07:59:05 +0000

    Feature/141/no results category (#181)
    * added no-results-found in Categories Page
    
    * delete onclick in NotFoundResults
    
    * finished not-found-results in categories page
    
    * added additional dependencies in useEffect Categories Page

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { styles } from '~/pages/categories/Categories.styles'
@@ -8,6 +8,7 @@ import OfferRequestBlock from '~/containers/find-offer/offer-request-block/Offer
 import AppToolbar from '~/components/app-toolbar/AppToolbar'
 import CategoriesList from '~/components/categories-list/CategoriesList'
 import DirectionLink from '~/components/direction-link/DirectionLink'
+import NotFoundResults from '~/components/not-found-results/NotFoundResults'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import SearchAutocomplete from '~/components/search-autocomplete/SearchAutocomplete'
 import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
@@ -19,6 +20,9 @@ import { CategoryNameInterface, ItemsWithCount, SizeEnum } from '~/types'
 const Categories = () => {
   const [match, setMatch] = useState<string>('')
   const [isFetched, setIsFetched] = useState<boolean>(false)
+  const [isSearchButtonClicked, setIsSearhButtonClicked] =
+    useState<boolean>(false)
+  const [isCategoryFound, setIsCategoryFound] = useState<boolean>(false)
   const { t } = useTranslation()
 
   const transform = useCallback(
@@ -43,6 +47,22 @@ const Categories = () => {
     setIsFetched(true)
   }
 
+  useEffect(() => {
+    if (match) {
+      const categoryArray = categoriesNamesItems.filter(
+        (category) => category === match
+      )
+      if (categoryArray.length === 0) {
+        setIsCategoryFound(true)
+      } else {
+        setIsCategoryFound(false)
+      }
+    }
+    if (!match) {
+      setIsCategoryFound(false)
+    }
+  }, [isSearchButtonClicked, categoriesNamesItems, match])
+
   return (
     <PageWrapper>
       <OfferRequestBlock />
@@ -62,6 +82,7 @@ const Categories = () => {
         <SearchAutocomplete
           loading={categoryNamesLoading}
           onFocus={getCategoriesNames}
+          onSearchChange={() => setIsSearhButtonClicked(!isSearchButtonClicked)}
           options={categoriesNamesItems}
           search={match}
           setSearch={setMatch}
@@ -70,6 +91,12 @@ const Categories = () => {
           }}
         />
       </AppToolbar>
+      {isCategoryFound && (
+        <NotFoundResults
+          buttonText={t('errorMessages.buttonRequest', { name: 'category' })}
+          description={t('errorMessages.tryAgainText', { name: 'category' })}
+        />
+      )}
       <CategoriesList query={match} />
     </PageWrapper>
   )
