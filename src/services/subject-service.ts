7ca403846e7d9c074d0ce5ab85807357115135c8
poggy33 <33poggy33@gmail.com>
2024-11-11 17:50:03 +0000

    solving subject service

--- src/services/subject-service.ts
+++ src/services/subject-service.ts
@@ -1,25 +1,17 @@
 import { AxiosResponse } from 'axios'
 
 import { axiosClient } from '~/plugins/axiosClient'
-import { createUrlPath } from '~/utils/helper-functions'
 import { URLs } from '~/constants/request'
-import { ItemsWithCount, SubjectInterface, SubjectNameInterface } from '~/types'
+import { ItemsWithCount, SubjectInterface, SubjectParamsInterface } from '~/types'
+
 
 export const subjectService = {
-  getSubjects: (
-    params?: Pick<SubjectInterface, 'name'>,
-    categoryId?: string
-  ): Promise<AxiosResponse<ItemsWithCount<SubjectInterface>>> => {
-    const category = createUrlPath(URLs.categories.get, categoryId)
-    return axiosClient.get(
-      `${URLs.subjects.get}${category}${URLs.subjects.get}`,
-      { params }
-    )
-  },
-  getSubjectsNames: (
-    categoryId: string | null
-  ): Promise<AxiosResponse<SubjectNameInterface[]>> => {
-    const category = createUrlPath(URLs.categories.get, categoryId)
-    return axiosClient.get(`${category}${URLs.subjects.getNames}`)
+  getSubjects: ( 
+    params?: Partial<SubjectParamsInterface>, 
+  ): Promise<AxiosResponse<ItemsWithCount<SubjectInterface>>> => { 
+  return axiosClient.get(URLs.subjects.get, { params } );
   }
 }
+
+
+
