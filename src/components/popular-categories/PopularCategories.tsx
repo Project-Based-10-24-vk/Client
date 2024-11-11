04aee5c28185189d280f63b7b64f9a53b50e7709
zim89 <121241841+zim89@users.noreply.github.com>
2024-11-11 12:47:21 +0200

    add link button to all categories

--- src/components/popular-categories/PopularCategories.tsx
+++ src/components/popular-categories/PopularCategories.tsx
@@ -1,16 +1,25 @@
 import { useEffect, useMemo, useState } from 'react'
+import App from '~/App'
 import type { AxiosResponse } from 'axios'
 import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
 
 import { Box, Stack, Typography } from '@mui/material'
 import { authRoutes } from '~/router/constants/authRoutes'
 import { categoryService } from '~/services/category-service'
-import type { CategoryInterface, ItemsWithCount } from '~/types'
+import {
+  ButtonVariantEnum,
+  SizeEnum,
+  type CategoryInterface,
+  type ItemsWithCount
+} from '~/types'
+import AppButton from '../app-button/AppButton'
 import CardWithLink from '../card-with-link/CardWithLink'
 import { styles } from './PopularCategories.styles'
 
 const PopularCategories = () => {
   const { t } = useTranslation()
+  const navigate = useNavigate()
   const [categories, setCategories] = useState<CategoryInterface[]>([])
 
   useEffect(() => {
@@ -48,7 +57,16 @@ const PopularCategories = () => {
   return (
     <Stack component='section' spacing={4}>
       <Typography variant='h4'>{t('header.categoriesPopular')}</Typography>
-      <Box sx={styles.cardsContainer}>{cardElements}</Box>
+      <Stack component='section' spacing='40px'>
+        <Box sx={styles.cardsContainer}>{cardElements}</Box>
+        <AppButton
+          onClick={() => navigate(authRoutes.categories.path)}
+          sx={{ width: '217px', height: '56px', alignSelf: 'center' }}
+          variant={ButtonVariantEnum.Tonal}
+        >
+          {t('button.viewAllCategories')}
+        </AppButton>
+      </Stack>
     </Stack>
   )
 }
