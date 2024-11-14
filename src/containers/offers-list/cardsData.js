c9133f0fa5d7503cf0bc931a35641e7177159585
AlexApiko <Alex@apiko.com>
2024-11-14 16:09:54 +0200

    update offer card list

---
+++ src/containers/offers-list/cardsData.js
@@ -0,0 +1,55 @@
+const mockData = [
+  {
+    id: 1,
+    name: 'Olivia Smith',
+    position: 'Associate Professor of Applied Linguistics',
+    rating: 4.8,
+    reviews: 15,
+    languages: ['Ukrainian', 'Spanish'],
+    course: {
+      subject: 'Spanish',
+      level: 'intermediate - advanced'
+    },
+    description:
+      "Hola! I specialize in teaching Spanish using interactive and immersive methods. Whether you're just starting or looking to polish your skills, I can help you become fluent in no time.",
+    price: '85 UAH',
+    perHour: '/hour',
+    avatarUrl: 'https://cdn.prod.website-files.com/6365d860c7b7a7191055eb8a/65a74dc1fe8e58cc1acb1f4e_Ava%20Wright.png'
+  },
+  {
+    id: 2,
+    name: 'Michael Johnson',
+    position: 'Head of English Language Studies',
+    rating: 5,
+    reviews: 22,
+    languages: ['English', 'French'],
+    course: {
+      subject: 'English',
+      level: 'beginner - expert'
+    },
+    description:
+      "Welcome! I have extensive experience in teaching English as a second language, focusing on conversational skills, business communication, and exam preparation (IELTS, TOEFL).",
+    price: '120 UAH',
+    perHour: '/hour',
+    avatarUrl: 'https://mdbcdn.b-cdn.net/img/new/avatars/2.webp'
+  },
+  {
+    id: 3,
+    name: 'Sophia Lee',
+    position: 'Lecturer in German Language and Literature',
+    rating: 4.9,
+    reviews: 18,
+    languages: ['German', 'Ukrainian'],
+    course: {
+      subject: 'German',
+      level: 'beginner - intermediate'
+    },
+    description:
+      "Guten Tag! With a passion for German culture and language, I provide engaging lessons that make learning German fun and effective, tailored to your level and goals.",
+    price: '95 UAH',
+    perHour: '/hour',
+    avatarUrl: 'https://t3.ftcdn.net/jpg/09/01/47/06/360_F_901470600_OmeKbMemJeYT5LHTwYqFGVUg97gfzCWP.jpg'
+  }
+];
+
+export default mockData;
