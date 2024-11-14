a130277bb2d49ea24537383a54183904b5ed7d41
Roman Nadraha <64159673+nadraharr@users.noreply.github.com>
2024-11-15 01:49:14 +0300

    tutor profile (#235)

--- src/services/subject-service.ts
+++ src/services/subject-service.ts
@@ -2,13 +2,18 @@ import { AxiosResponse } from 'axios'
 
 import { axiosClient } from '~/plugins/axiosClient'
 import { URLs } from '~/constants/request'
-import { ItemsWithCount, SubjectInterface, SubjectParamsInterface, SubjectNameInterface } from '~/types'
+import {
+  ItemsWithCount,
+  SubjectInterface,
+  SubjectNameInterface,
+  SubjectParamsInterface
+} from '~/types'
 
 export const subjectService = {
-  getSubjects: ( 
-    params?: Partial<SubjectParamsInterface>, 
-  ): Promise<AxiosResponse<ItemsWithCount<SubjectInterface>>> => { 
-  return axiosClient.get(URLs.subjects.get, { params } )
+  getSubjects: (
+    params?: Partial<SubjectParamsInterface>
+  ): Promise<AxiosResponse<ItemsWithCount<SubjectInterface>>> => {
+    return axiosClient.get(URLs.subjects.get, { params })
   },
   getSubjectsWithParamsAndCategoryId: (
     params?: Pick<SubjectInterface, 'name'>,
@@ -23,6 +28,3 @@ export const subjectService = {
     return axiosClient.get(`${URLs.subjects.getNames}?category=${categoryId}`)
   }
 }
-
-
-
